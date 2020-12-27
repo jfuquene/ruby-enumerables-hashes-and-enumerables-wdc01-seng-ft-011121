8055a52c072cee9d26eaf25f9fba9359cff5e93e
John Fuquene <jfuquene@email.wm.edu>
2020-12-27 17:44:41 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,6 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are nore #{age} years old!"
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end 
-  # add your code snippet here!
 end
